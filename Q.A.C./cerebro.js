e2c52f6472b77dafe13c5844f25f2bf34773acc3
LuisGabrielCananPh <150362762+LuisGabrielCananPh@users.noreply.github.com>
2023-11-17 09:06:02 -0300

    Add files via upload

--- Q.A.C./cerebro.js
+++ Q.A.C./cerebro.js
@@ -1,5 +1,6 @@
 
 
+var pontos = 0
 var links = new Array(5)
 
 links[1] = "eq_0.html" 
@@ -8,22 +9,38 @@ links[3] = "eq_2.html"
 links[4] = "eq_3.html" 
 links[5] = "eq_4.html" 
 
-function easy() { 
+function easy(){ 
+    
     location = sorteio(); 
 }
  
 function lose(){
     location = "lose.html"
+    pontos = 0
+    console.log(pontos)
+    
 }
 function voltar(){
     location = "lobby.html"
 }
 
 
-function won() {
-    sorteio();
-    //location = sorteio() 
-} 
+
+function won(){
+    location = sorteio()
+    console.log(pontos)
+    let pontos = (pontos + 1)
+    if(5<pontos){ 
+        location = "win.html"     
+}
+    
+        
+}
+function end(){
+
+}
+
+ 
 
 
 function sorteio(){
@@ -33,6 +50,7 @@ function sorteio(){
     return location = links[i];
     */
    
+    /** 
     var maximo = 5;
 
     var i, arr = [];
@@ -47,9 +65,30 @@ function sorteio(){
         arr[n] = arr[p];
         arr[p] = tmp;
     }
+    console.log("p="+p+" n="+n+" tmp="+tmp)
 
-    return 
-       
+    return links[tmp]
+    */ 
+    var tmp;
+    const quantidade = 1, minimo = 1, maximo = 5;
+    const todos = [];
+    for (let i = minimo; i <= maximo; i++) { 
+        // gera um array com todos os números
+        todos.push(i);
+    }
+// embaralha as 6 primeiras posições
+    for (let i = 0; i < quantidade; i++) {
+    // pega uma posição aleatória do array e troca com a posição atual
+        const j = Math.floor(Math.random() * todos.length);
+        const tmp = todos[j];
+        todos[j] = todos[i];
+        todos[i] = tmp;
+    }
+// pega os 6 primeiros elementos
+        const result = todos.slice(0, quantidade);
+        console.log("result="+result)
+        return links[result]
+
 }
 
 
