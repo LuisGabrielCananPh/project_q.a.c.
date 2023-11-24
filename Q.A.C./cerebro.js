4b836186ae5abfb48d1739f474c6e60ca9648a36
LuisGabrielCananPh <150362762+LuisGabrielCananPh@users.noreply.github.com>
2023-11-24 09:13:20 -0300

    Add files via upload

--- Q.A.C./cerebro.js
+++ Q.A.C./cerebro.js
@@ -1,44 +1,60 @@
 
-
+var q = 0
 var pontos = 0
 var links = new Array(5)
+var sugestao = 0
 
 links[1] = "eq_0.html" 
 links[2] = "eq_1.html" 
 links[3] = "eq_2.html" 
 links[4] = "eq_3.html" 
 links[5] = "eq_4.html" 
+links[6] = "eq_5.html" 
+links[7] = "eq_6.html" 
+links[8] = "eq_7.html"
+links[9] = "eq_8.html"
 
 function easy(){ 
     
-    location = sorteio(); 
+    location = sorteio();
+     
 }
  
 function lose(){
     location = "lose.html"
     pontos = 0
+    q = 0
     console.log(pontos)
     
 }
 function voltar(){
     location = "lobby.html"
 }
-
+console.log("pontos="+pontos)
+    console.log("pergunta="+sugestao)
 
 
 function won(){
-    location = sorteio()
-    console.log(pontos)
-    let pontos = (pontos + 1)
-    if(5<pontos){ 
-        location = "win.html"     
-}
-    
-        
-}
-function end(){
+    pontos = (pontos + 1)
+    q = (q + 1)
+    location = next()
+    console.log("pontos="+pontos)
+    console.log("pergunta="+sugestao)
 
+
+}   
+ function end(){
+    if(9<pontos){ 
+        location = "win.html"
+    }
+ }       
+function next(){
+    let q = sorteio()
+    q = numero.slice(0, quantidade);
+
+    return links[numero[q]]
 }
+
 
  
 
@@ -68,27 +84,50 @@ function sorteio(){
     console.log("p="+p+" n="+n+" tmp="+tmp)
 
     return links[tmp]
-    */ 
+    */
+   
     var tmp;
-    const quantidade = 1, minimo = 1, maximo = 5;
+    const quantidade = 9, minimo = 1, maximo = 9;
     const todos = [];
     for (let i = minimo; i <= maximo; i++) { 
-        // gera um array com todos os números
+        
         todos.push(i);
     }
-// embaralha as 6 primeiras posições
+
     for (let i = 0; i < quantidade; i++) {
-    // pega uma posição aleatória do array e troca com a posição atual
+    
         const j = Math.floor(Math.random() * todos.length);
         const tmp = todos[j];
         todos[j] = todos[i];
         todos[i] = tmp;
     }
-// pega os 6 primeiros elementos
-        const result = todos.slice(0, quantidade);
-        console.log("result="+result)
-        return links[result]
 
+        const result = todos.slice(0, quantidade);
+        
+        console.log("result="+result);
+
+        
+
+        return result[todos[q]];
+         
+        /*
+        var sorteados = [];
+        var valorMaximo = 9;
+
+    if (sorteados.length == valorMaximo) {
+        if (confirm('Já não há mais! Quer recomeçar?')) sorteados = [];
+        else return;
+    }
+     sugestao = Math.ceil(Math.random() * valorMaximo); // Escolher um numero ao acaso
+    while (sorteados.indexOf(sugestao) >= 0) {  // Enquanto o numero já existir, escolher outro
+        sugestao = Math.ceil(Math.random() * valorMaximo);
+    }
+    sorteados.push(sugestao); // adicionar este numero à array de numeros sorteados para futura referência
+    
+    
+    return links[sugestao]; // devolver o numero único
+    */
 }
+
 
 
